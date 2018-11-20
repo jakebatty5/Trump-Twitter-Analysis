fc8d01a49677226cd455c2091290ffda72fa8191
jakebatty5 <jbatty@ncsu.edu>
2018-11-19 20:41:46 -0500

    added API access via config file

---
+++ config.py
@@ -0,0 +1,11 @@
+def consumer_key():
+	return 'iYuXDDCGpHK21IKsdPHp2SGyC'
+
+def consumer_secret():
+	return 'WS0VRAEgucLPMUC4sPHkR6paNWUWjnQMD9Sa69dLMsbYnSoGSl'
+
+def access_token():
+	return '1059635773832654849-dXbjvut3bhqxSiz3ELj1mVW3PN6qXR'
+
+def access_token_secret():
+	return 'xDOSZMwZkBZrlDMjrVA7liAcnS0zCxu6qchzqdOOJr6AL'
--- load_general_tweets.py
+++ load_general_tweets.py
@@ -5,16 +5,13 @@ import json
 import re
 from textblob import TextBlob
 import pandas as pd
+import config
 
 def clean_text(message):
 	return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", message).split())
 
-consumer_key = 'iYuXDDCGpHK21IKsdPHp2SGyC'
-consumer_secret = 'WS0VRAEgucLPMUC4sPHkR6paNWUWjnQMD9Sa69dLMsbYnSoGSl'
-access_token = '1059635773832654849-dXbjvut3bhqxSiz3ELj1mVW3PN6qXR'
-access_token_secret = 'xDOSZMwZkBZrlDMjrVA7liAcnS0zCxu6qchzqdOOJr6AL'
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+auth = tweepy.OAuthHandler(config.consumer_key(), config.consumer_secret())
+auth.set_access_token(config.access_token(), config.access_token_secret())
 
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
@@ -37,4 +34,4 @@ for tweet in tweepy.Cursor(api.search, lang='en', q="a").items():
 		sentiment = 'neutral'
 	cur.execute(general_sql, (tweet.text, polarity, subjectivity, sentiment))
 	count = count + 1
-	if count > 10000: break
+	if count > 5: break
